d2a5a5cc9a2c09c95ea914d8dc5c0048fb28dfb5
Wizer <simon.tessier8@gmail.com>
2021-03-23 19:22:19 +0100

    Footer link

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="footer">
-    <div id="effect_holder">
+    <div id="effect_holder" data-scroll data-scroll-speed="1" data-scroll-direction="horizontal">
       <p id="mouse_over">      
         Merci du passage
       </p>
@@ -10,7 +10,18 @@
       <p id="bot_text">      
         Merci du passage
       </p>
-    </div>
+    </div>    
+    <p id="end" data-scroll data-scroll-speed="-6" data-scroll-direction="horizontal">
+      Si tu à apprécier la promenade, viens jeter un oeil à mon porte-folio
+    </p>
+    <a id="portefolio" href="https://wizer21.github.io/" target="_blank" data-scroll data-scroll-speed="3" data-scroll-direction="horizontal">
+      <p id="portefolio_text_border">
+        Jeter un oeil
+      </p>
+      <p id="portefolio_text">
+        Jeter un oeil
+      </p>
+    </a>
   </div>
 </template>
 
@@ -31,6 +42,14 @@ export default {
       top_text.style.clipPath = `circle(5% at ${event.offsetX / x_converter}% ${event.offsetY / y_converter}%`
     })
     
+    let portefolio = document.getElementById('portefolio')
+    let portefolio_text = document.getElementById('portefolio_text')
+    portefolio.addEventListener('mouseenter', () => {
+      portefolio_text.style.clipPath = "inset(0% 0 0 0)"
+    })    
+    portefolio.addEventListener('mouseleave', () => {
+      portefolio_text.style.clipPath = "inset(75% 0 0 0)"
+    })
   }
 }
 </script>
@@ -63,14 +82,14 @@ export default {
 }
 #mouse_over
 {
-  -webkit-text-stroke: 1px #2f4d2c;
+  -webkit-text-stroke: 1px #345c30;
   color: transparent;
   cursor: none;
 }
 #top_text
 {
-  -webkit-text-stroke: 1px #2f4d2c;
-  color: #2f4d2c;
+  -webkit-text-stroke: 1px #345c30;
+  color: #345c30;
   clip-path: circle(0% at 0% 0%);
   pointer-events: none;
 
@@ -78,10 +97,46 @@ export default {
 }
 #bot_text
 {
-  -webkit-text-stroke: 1px #2f4d2c;
-  color: #2f4d2c;
+  -webkit-text-stroke: 1px #345c30;
+  color: #345c30;
   animation: var(--float_anim) 1s infinite ease-in-out alternate;
   pointer-events: none;
+}
+#end
+{
+  font-size: 1em;
+  margin-right: auto;
+  margin-left: 10vw;
+}
+#portefolio
+{
+  margin-left: auto;
+  margin-right: 30vw;
+  text-decoration: none;
+
+  display: grid;
+}
+#portefolio_text_border
+{
+  -webkit-text-stroke: 1px #345c30;
+  color: transparent;
+  font-size: 4em;
+  margin: 0px;
+
+  grid-column: 1;
+  grid-row: 1;
+}
+#portefolio_text
+{
+  -webkit-text-stroke: 1px #2f4d2c;
+  color: #2f4d2c;
+  font-size: 4em;
+  grid-column: 1;
+  grid-row: 1;
+  margin: 0px;
+
+  transition-duration: 1s;
+  clip-path: inset(75% 0 0 0);
 }
 </style>
 
